f16d7720541cc2da21441f3f48ef260dc26a3298
Jaimin <jaiminpatel91@gmail.com>
2017-09-27 13:15:54 -0400

    add login component

---
+++ mintex-app/src/app/app.login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './app.login.component.html',
+  styleUrls: ['./app.login.component.css']
+})
+export class LoginComponent {
+}
--- mintex-app/src/app/app.module.ts
+++ mintex-app/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+import { LoginComponent } from './app.login.component'
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/logIn', pathMatch: 'full'},
+  { path: 'logIn', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+      RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
